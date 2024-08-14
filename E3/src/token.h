9370ecd5cd657bd8758e0d7b81221f89704f03a2
Claudio faria <claudio.dantas18@gmail.com>
2024-08-14 12:02:25 -0300

    completo

--- E3/src/token.h
+++ E3/src/token.h
@@ -2,12 +2,32 @@
 
 typedef enum {
         ID = 256, 
-	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
+        BOOLTYPE = 259,
+        CHARTYPE = 260,
+        ELSE = 261,
+        FALSE = 262,
+        FOR = 263,
+        FUNCTION = 264,
+        IF = 265,
+        INTTYPE = 266,
+        PRINT = 267,
+        RETURN = 268,
+        STRTYPE = 269,
+        TRUE = 270,
+        VOIDTYPE = 271,
+        WHILE = 272,
+        LEQ = 273,
+        GEQ = 274,
+        EQ = 275,
+        NEQ = 276,
+        AND = 277,
+        OR = 278,
+        
+	ARRAY,
         CHAR,
 	STR,
-	ERROR
+        NUM,
+        ERROR
+
 } token_t; 
 
